95b1b0b4340d28a4b5b9bebc96ad0d5c506f3aab
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-25 13:23:42 -0600

    list count

--- src/js/TodoSearch.js
+++ src/js/TodoSearch.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "../styles/TodoSearch.css";
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("");
-  console.log(searchValue);
-
+function TodoSearch({ searchValue, setSearchValue }) {
   return (
     <input
       placeholder="Buscar tarea"
       className="TodoSearch"
       value={searchValue}
       onChange={(event) => {
-        // console.log("Escribiste en el search");
         setSearchValue(event.target.value);
       }}
     />
