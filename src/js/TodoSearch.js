faddf6d327ebb3743fdf12d455e1a73a24d3dd40
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-25 12:27:21 -0600

    search status

--- src/js/TodoSearch.js
+++ src/js/TodoSearch.js
@@ -1,13 +1,18 @@
+import React from "react";
 import "../styles/TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+  console.log(searchValue);
+
   return (
     <input
       placeholder="Buscar tarea"
       className="TodoSearch"
+      value={searchValue}
       onChange={(event) => {
         // console.log("Escribiste en el search");
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
