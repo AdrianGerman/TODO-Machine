bacf3dacd3095726ec4fc099fcdd3bf2890ecb41
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-26 17:56:32 -0600

    localStorage data persistence

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import { TodoItem } from "./js/TodoItem";
 import { CreateTodoBtn } from "./js/CreateTodoBtn";
 import React from "react";
 
-const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Terminar curso React", completed: false },
-  { text: "Chambiarle", completed: false },
-  { text: "Dormir mas de 45 horas en un solo día", completed: false },
-];
+// const defaultTodos = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Terminar curso React", completed: false },
+//   { text: "Chambiarle", completed: false },
+//   { text: "Dormir mas de 45 horas en un solo día", completed: false },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.removeItem('TODOS_V1');
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -25,6 +38,11 @@ function App() {
     return todoText.includes(searchText);
   });
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem("TODOS_V1", JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
   // console.log(searchValue);
   const completeTodo = (text) => {
     const newTodos = [...todos];
@@ -32,7 +50,7 @@ function App() {
       (todo) => todo.text == text
     ));
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -41,7 +59,7 @@ function App() {
       (todo) => todo.text == text
     ));
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
