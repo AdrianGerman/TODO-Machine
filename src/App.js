2fb9f8909e73ea58d7a98549775bdd9aee23db14
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-27 19:06:49 -0600

    order folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { TodoCounter } from "./js/TodoCounter";
-import { TodoSearch } from "./js/TodoSearch";
-import { TodoList } from "./js/TodoList";
-import { TodoItem } from "./js/TodoItem";
-import { CreateTodoBtn } from "./js/CreateTodoBtn";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { TodoItem } from "./components/TodoItem";
+import { CreateTodoBtn } from "./components/CreateTodoBtn";
 import React from "react";
+
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 // const defaultTodos = [
 //   { text: "Cortar cebolla", completed: true },
@@ -14,27 +16,6 @@ import React from "react";
 
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-
-  const [item, setItem] = React.useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  };
-
-  return [item, saveItem];
-}
 
 function App() {
   const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
